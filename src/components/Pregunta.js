5b8fe3bb019aaef4e40b31ed5c546c7775664183
larrygf02 <raulgf_02@hotmail.com>
2019-10-31 16:07:10 -0500

    Initial commit

---
+++ src/components/Pregunta.js
@@ -0,0 +1,20 @@
+import React, { Fragment } from 'react'
+
+function Pregunta() {
+    return (
+        <Fragment>
+            <h2>Coloca tu presupuesto</h2>
+            <form>
+                <input type="number"
+                        className="u-full-width"
+                        placeholder="Agrega tu presupuesto"
+                        //onChange={}
+                        />
+
+                <input type="submit" className="button-primary u-full-width" value="Definir presupuesto"/>
+            </form>
+        </Fragment>
+    )
+}
+
+export default Pregunta

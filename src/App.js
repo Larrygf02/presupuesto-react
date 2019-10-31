48526a78177715fb1eca5ef2371a5c693216cd63
larrygf02 <raulgf_02@hotmail.com>
2019-10-31 16:19:01 -0500

    Agregando el presupuesto

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Pregunta from './components/Pregunta'
 
 function App() {
--- src/components/Pregunta.js
+++ src/components/Pregunta.js
@@ -1,13 +1,32 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useState } from 'react'
 
-function Pregunta() {
+function Pregunta(props) {
+    const { guardarPresupuesto } = props
+    const [ cantidad, guardarCantidad ] = useState(0);
+    const [ error, guardarError ] = useState(false);
+
+    const agregarPresupuesto = e => {
+        e.preventDefault();
+        //validar
+        if (cantidad <= 0 || isNaN(cantidad)) {
+            guardarError(true);
+            return;
+        }
+
+        // si se pasa la validacion pasar el presupuesto
+        guardarError(false)
+        guardarPresupuesto(cantidad)
+    }
     return (
         <Fragment>
             <h2>Coloca tu presupuesto</h2>
-            <form>
+            {error ? <p className="alert alert-danger error">El presupuesto es incorrecto</p>: null}
+            <form
+                onSubmit={agregarPresupuesto}>
                 <input type="number"
                         className="u-full-width"
                         placeholder="Agrega tu presupuesto"
+                        onChange={e => guardarCantidad(parseInt(e.target.value, 10))}
                         //onChange={}
                         />
 
